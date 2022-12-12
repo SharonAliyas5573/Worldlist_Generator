2ee880211832eeb96c3411f16b157c257134a82b
SharonAliyas5573 <116248803+SharonAliyas5573@users.noreply.github.com>
2022-12-12 23:25:08 +0530

    formate changes

--- worldlist_generator.py
+++ worldlist_generator.py
@@ -1,15 +1,14 @@
 import string
 from itertools import product
-# min_len=int(input("Enter minimum length of PASSWORD :"))
-# max_len=int(input("Enter maximum length of PASSWORD :"))
 
-F_nme=input(" Enter First name :")
-L_nme=input(" Enter Last name :")
-p_nme=input(" Enter pet's name :")
-ph=input(" Enter  Phone number :")
-ph2=input(" Enter First 4 digit of Phone number :")
-ph1=input(" Enter last 4 digit of Phone number :")
-Dob=input(" Enter DOY :")
+
+F_nme = input(" Enter First name :")
+L_nme = input(" Enter Last name :")
+p_nme = input(" Enter pet's name :")
+ph = input(" Enter  Phone number :")
+ph2 = input(" Enter First 4 digit of Phone number :")
+ph1 = input(" Enter last 4 digit of Phone number :")
+Dob = input(" Enter DOY :")
 Data = []
 Data.append(F_nme)
 Data.append(L_nme)
@@ -17,6 +16,7 @@ Data.append(p_nme)
 Data.append(ph1)
 Data.append(ph)
 Data.append(Dob)
+
 
 def extra_info():
     opt = input("Do you Want to ADD more info ? [Y/N]")
@@ -31,7 +31,7 @@ def extra_info():
             opt = input("Do you Want to ADD more info ? [Y/N]")
 
     elif opt == 'N' or opt == 'n':
-        data=[]
+        data = []
     else:
         print(' wrong input ')
 
@@ -40,23 +40,20 @@ def extra_info():
 
 val = extra_info()
 
-Data=Data+val
+Data = Data+val
 
-# Data1=list(string.ascii_uppercase + string.ascii_lowercase + string.digits)
-Data2=['@','#','$','*','&','_','-']
-Data=Data+Data2
-counter=0
 
-file_open = open("Wordlist.txt",'w')    
-for i in range(1,4):
-    for j in product(Data,repeat=i):
-       list="".join(j)
-       file_open.write(list)
-       file_open.write('\n')
-       
-       counter +=1
+Data2 = ['@', '#', '$', '*', '&', '_', '-']
+Data = Data+Data2
+counter = 0
+
+file_open = open("Wordlist.txt", 'w')
+for i in range(1, 4):
+    for j in product(Data, repeat=i):
+        list = "".join(j)
+        file_open.write(list)
+        file_open.write('\n')
+
+        counter += 1
 
 print(f'WORDLIST WITH {counter} PASSWORDS GENERATED')
-
-
-
